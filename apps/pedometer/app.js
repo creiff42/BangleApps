a93ef3ad0f7154208be9b5320b9ed0572218db55
creiff42 <82869506+creiff42@users.noreply.github.com>
2023-12-29 09:20:24 +0100

    Create pedometer.app.js

---
+++ apps/pedometer/app.js
@@ -0,0 +1,12 @@
+g.setFontAlign(0,0); // center font
+g.setFont("Vector",80);
+
+var steps = Bangle.getHealthStatus("day").steps;
+
+function showSteps() {
+  g.clear();
+  g.drawString(steps, g.getWidth()/2, g.getHeight()/2);
+  Bangle.setLCDPower(1);
+}
+
+var interval = setInterval(showSteps, 1000);
